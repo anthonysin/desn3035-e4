7c77b64fef0f18d9bf9c4b371f0ef0cdbdc089ee
Anthony Singh <101334291@georgebrown.ca>
2024-10-08 18:35:55 +0000

    Error fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "./components/header";
 import Nav from "./components/nav";
-import About from "./about/about";
 
 export default function Page() {
     return <>
